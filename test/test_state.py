968b2091542db467ff00643ea7c22054c14fd26f
Dima Tisnek <dimaqq@gmail.com>
2017-05-10 13:41:50 +0200

    Who makes next move?

--- test/test_state.py
+++ test/test_state.py
@@ -3,10 +3,23 @@ import ttt.state
 
 
 def test_validate():
+    ttt.state.validate([[None]])
     ttt.state.validate([[0]])
+    ttt.state.validate([list("xox"), [None] * 3, [None] * 3])
 
     with pytest.raises(AssertionError):
         ttt.state.validate([]), "board size must be > 0"
 
     with pytest.raises(AssertionError):
         ttt.state.validate([[None], [None], [None]]), "board must be square"
+
+    with pytest.raises(AssertionError):
+        ttt.state.validate([[1, 2, 1], [None, None, None], [1, 2, 3]]), "can't have 3 players"
+
+
+def test_next_move():
+    assert ttt.state.next_move([[None]]) == "x"
+
+    assert ttt.state.next_move([["x", None], [None, None]]) == "o"
+    assert ttt.state.next_move([["x", "o"], [None, None]]) == "x"
+    assert ttt.state.next_move([["x", "o"], ["x", None]]) == "o"
--- ttt/state.py
+++ ttt/state.py
@@ -2,8 +2,49 @@
     Initial state is all None.
     Running state will have a few slots populated by players (ids or objects)
 """
+from collections import Counter
 
 
 def validate(state):
     assert state
     assert len(state) == len(state[0])
+    c = sum(map(Counter, state), Counter())
+
+    # possible records are None, player1, player2
+    assert 1 <= len(c) <= 3
+
+    players = [k for k in c.keys() if k is not None]
+    assert len(players) <= 2
+
+    if players:
+        if len(players) > 1:
+            p1, p2 = players
+            assert abs(c[p1] - c[p2]) <= 1
+        else:
+            p1, = players
+            assert c[p1] <= 1
+
+
+def next_move(state, players="xo"):
+    c = sum(map(Counter, state), Counter())
+    board_players = [k for k in c.keys() if k is not None]
+    if not board_players:
+        return players[0]
+    if len(board_players) == 1:
+        p1, = board_players
+        assert p1 == players[0]
+
+        return players[1]
+    else:
+        p1, p2 = board_players
+        assert p1 in players
+        assert p2 in players
+
+        if c[p1] == c[p2]:
+            return players[0]
+        else:
+            return sorted(board_players, key=lambda k: c[k])[0]
+
+
+def game_over(state):
+    ...
